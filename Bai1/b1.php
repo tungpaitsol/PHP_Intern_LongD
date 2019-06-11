d629401f9ab79ccec3049cf62f74f4b5d675ddd9
LongD <dinhlong.utt@gmail.com>
2019-06-11 14:57:04 +0700

    edit lai bai 1

--- Bai1/b1.php
+++ Bai1/b1.php
@@ -10,49 +10,41 @@
 		<form action="b1.php" method="POST" name="ptbh">
 			<div class="form-group">
 				<label for="numbera"> Nhập Số A</label>
-				<input type="number" name="nba" id="numbera" class="form-control">
+				<input type="text" name="nba" id="numbera" class="form-control">
 			</div>
 			<div class="form-group">
 				<label for="numberb"> Nhập Số B</label>
-				<input type="number" name="nbb" id="numberb" class="form-control">
+				<input type="text" name="nbb" id="numberb" class="form-control">
 			</div>
 			<div class="form-group">
 				<label for="numberc"> Nhập Số C</label>
-				<input type="number" name="nbc" id="numberc" class="form-control">
+				<input type="text" name="nbc" id="numberc" class="form-control">
 			</div>
 			
-			<button type="submit" class="btn btn-primary">Tinh</button>
+			<button type="submit" class="btn btn-primary">Tính</button>
 		</form>
 	
 
 	<?php
 
-		if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["nba"]) && !empty($_POST["nbb"]) && !empty($_POST["nbc"])) {
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 			$nba = $_POST["nba"];
 			$nbb = $_POST["nbb"];
-			$nbc = $_POST["nbc"];
 
-			if (check_input($nba) && check_input($nbb) && check_input($nbc)) {				
+			$nbc = (strlen($_POST["nbc"]) == "") ? 0 : $_POST["nbc"];
+
+			if ($nba != 0 && (is_numeric($nba) && is_numeric($nbb)  && is_numeric($nbc))) {
 				echo delta($nba, $nbb, $nbc);
 			} else {
-				echo "Nhap phai la so";
+				echo("Sai dinh dang!");
 			}
-
-		} else {
-			echo "Nhap lai";
-		}
-
-		function check_input($var) {
-			//var_dump($var);			
-			return is_numeric($var);
-			
 		}
 
 		function delta($a, $b, $c) {
 			$del = ($b*$b) - (4*$a*$c);
-			//echo $a . $b . $c . $del;
-			$res = "";
+			$res = null;
+
 			if ($del < 0) {
 				$res = "Phuong trinh vo nghiem";
 			} elseif ($del == 0) {
