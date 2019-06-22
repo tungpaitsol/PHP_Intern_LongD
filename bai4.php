0f1cb3972a500ae31f3f9ffe97ae5588d6858c49
LongD <dinhlong.utt@gmail.com>
2019-06-22 11:23:56 +0700

    edit new

--- bai4.php
+++ bai4.php
@@ -1,141 +1,161 @@
 <?php
-  session_start();
-  $data_products = empty($_SESSION['products']) ? createProduct() : $_SESSION['products'];
+    session_start();
+    $_SESSION['products'] = empty($_SESSION['products']) ? createProduct() : $_SESSION['products'];
+    $products = $_SESSION['products'];
 
-  // foreach ($_POST as $name => $value) {
-  //    var_dump($name);
-  //    var_dump($value);
-  // }
+    if (isset($_POST['btnCreateProducts'])) {
+        $get_error = checkErr($_POST['inputIndexed']);
+        if ($get_error['stt'] === 1) {
+            $products = createProduct($_POST['inputIndexed']);
+            $_SESSION['products'] = $products;
+        }
+    }
+    
+    if (isset($_POST['btnID'])) {
+        $type_id = $_POST['btnID'] == SORT_DESC ? SORT_ASC : SORT_DESC;
+        $products = quickSort($products, $type_id, 'id');
+    }
 
-  if (isset($_POST['btnCreateProducts'])) {
-    $data_products = createProduct();
-  }
+    if (isset($_POST['btnName'])) {
+        $type_name = $_POST['btnName'] == SORT_DESC ? SORT_ASC : SORT_DESC;
+        $products = quickSort($products, $type_name, 'name');
+    }
 
-  if (isset($_POST['btnID'])) {
-    $data_products = (sortProduct($data_products, 'id'));
-  }
+    if (isset($_POST['btnPrince'])) {
+        $type_prince = $_POST['btnPrince'] == SORT_DESC ? SORT_ASC : SORT_DESC;
+        $products = quickSort($products, $type_prince, 'prince');
+    }
 
-  if (isset($_POST['btnName'])) {
-    $data_products = (sortProduct($data_products, 'name'));
-  }
+    if (isset($_POST['btnQuantity'])) {
+        $type_quantity = $_POST['btnQuantity'] == SORT_DESC ? SORT_ASC : SORT_DESC;
+        $products = quickSort($products, $type_quantity, 'quantity');        
+    }
 
-  if (isset($_POST['btnPrince'])) {
-    $data_products = (sortProduct($data_products, 'prince'));
-  }
+    if (isset($_POST['btnOrder'])) {
+        $type_order = $_POST['btnOrder'] == SORT_DESC ? SORT_ASC : SORT_DESC;
+        $products = quickSort($products, $type_order, 'order');
+    }
 
-  if (isset($_POST['btnQuantity'])) {
-    $data_products = (sortProduct($data_products, 'quantity'));
-  }
+    if (isset($_POST['btnTotal'])) {
+        $type_total = $_POST['btnTotal'] == SORT_DESC ? SORT_ASC : SORT_DESC;
+        $products = quickSort($products, $type_total, 'total');
+    }
 
-  if (isset($_POST['btnOrder'])) {
-    $data_products = (sortProduct($data_products, 'order'));
-  }
-
-  if (isset($_POST['btnTotal'])) {
-    $data_products = (sortProduct($data_products, 'total'));
-  }
-
-  $_SESSION['products'] = $data_products;
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <title>Bài 4</title>
-  <style type="text/css">
-    tr .btn {
-      font-weight: bold;
-      width: 100%;
-    }
-  </style>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Bài 4</title>
+    <style type="text/css">
+        tr .btn {
+          font-weight: bold;
+          width: 100%;
+        }
+    </style>
 </head>
 <body>
-  <div class="container" style="margin: 50px auto;width: 70%">
+<div class="container" style="margin: 50px auto;width: 70%">
     <form action="" method="POST">
-      <table class="table table-hover table-striped table-sm table-bordered">
-        <thead>
-          <tr>
-            <th scope="col"><input type="submit" class="btn" value="ID" name="btnID"></th>
-            <th scope="col"><input type="submit" class="btn" value="Name" name="btnName"></th>
-            <th scope="col"><input type="submit" class="btn" value="Prince" name="btnPrince"></th>
-            <th scope="col"><input type="submit" class="btn" value="Quantity" name="btnQuantity"></th>
-            <th scope="col"><input type="submit" class="btn" value="Order" name="btnOrder"></th>
-            <th scope="col"><input type="submit" class="btn" value="Total" name="btnTotal"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($data_products['products'] as $product): ?>
-          <tr>
-              <td scope="row"><?php echo($product['id']) ?></td>
-              <td><?php echo($product['name']) ?></td>
-              <td><?php echo($product['prince']) ?></td>
-              <td><?php echo($product['quantity']) ?></td>
-              <td><?php echo($product['order']) ?></td>  
-              <td><?php echo($product['total']) ?></td>          
-          </tr>
-          <?php endforeach ?>
-        </tbody>
-      </table>
-      <div class="form-inline">
-          <!-- <input type="text" class="form-control" name="inputIndexed"> -->
-          <input type="submit" name="btnCreateProducts" value="Tạo mới" class="btn btn-primary">
-      </div>      
+        <table class="table table-hover table-striped table-sm table-bordered">
+            <thead>
+                <tr>            
+                    <th scope="col">
+                        <button type="submit" class="btn" value="<?php echo $type_id ?? 4; ?>" name="btnID">ID</button>
+                    </th>
+                    <th scope="col">
+                        <button type="submit" class="btn" value="<?php echo $type_name ?? 4; ?>" name="btnName">Name</button>
+                    </th>
+                    <th scope="col">
+                        <button type="submit" class="btn" value="<?php echo $type_prince ?? 4; ?>" name="btnPrince">Prince</button>
+                    </th>
+                    <th scope="col">
+                        <button type="submit" class="btn" value="<?php echo $type_quantity ?? 4; ?>" name="btnQuantity">Quantity</button>
+                    </th>
+                    <th scope="col">
+                        <button type="submit" class="btn" value="<?php echo $type_order ?? 4; ?>" name="btnOrder">Order</button>
+                    </th>
+                    <th scope="col">
+                        <button type="submit" class="btn" value="<?php echo $type_total ?? 4; ?>" name="btnTotal">Total</button>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if(isset($products)) foreach ($products as $product): ?>
+                <tr>
+                    <td scope="row"><?php echo($product['id']) ?></td>
+                    <td><?php echo($product['name']) ?></td>
+                    <td><?php echo($product['prince']) ?></td>
+                    <td><?php echo($product['quantity']) ?></td>
+                    <td><?php echo($product['order']) ?></td>  
+                    <td><?php echo($product['total']) ?></td>          
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+        <div class="form-inline">
+            <input type="number" class="form-control" name="inputIndexed">
+            <input type="submit" name="btnCreateProducts" value="Tạo mới" class="btn btn-primary" style="margin: auto 5px">
+            <?php if(!empty($get_error['message'])) { echo $get_error['message'];}?>
+        </div>    
     </form>
     
     <?php
 
-      function sortProduct($data_products, $column):array
-      {
-        $arr_products = $data_products['products'];
-        $arr_type = $data_products['sort_type'];
-        $type = array_key_exists($column, $arr_type) ? ($arr_type[$column] == SORT_ASC ? SORT_DESC : SORT_ASC) : SORT_ASC;
-        // $column_product = array_column($arr_products, $column);
-        // array_multisort($column_product, $type, $arr_products);
-        $arr_products = quickSort($arr_products, $type, $column);
-        $arr_type = array($column => $type);
-        return array('products' => $arr_products, 'sort_type' => $arr_type);
-      }
-
-      function quickSort($array_input, $array_sort_order, $column_key):array
-      {        
-        if (count($array_input) <= 1) {
-          return $array_input;
-        }
-
-        $pivot = $array_input[0][$column_key];
-        $left = array();
-        $right = array();
-
-        for($i = 1; $i < count($array_input); $i++)
+        function quickSort($array_input, $sort_type, $column_key):array
         {
-          if($array_input[$i][$column_key] < $pivot) {
-            $array_sort_order == SORT_ASC ? array_push($left, $array_input[$i]) : array_push($right, $array_input[$i]);
-          } else {
-            $array_sort_order == SORT_DESC ? array_push($left, $array_input[$i]) : array_push($right, $array_input[$i]);
-          }
+            if (count($array_input) <= 1) {
+              return $array_input;
+            }
+
+            $pivot = $array_input[0][$column_key];
+            $left = array();
+            $right = array();
+
+            for($i = 1; $i < count($array_input); $i++)
+            {
+                if($array_input[$i][$column_key] < $pivot) {
+                    $sort_type == SORT_ASC ? $left[] = $array_input[$i] : $right[] = $array_input[$i];
+                    continue;
+                }
+
+                $sort_type == SORT_DESC ? $left[] = $array_input[$i] : $right[] = $array_input[$i];                
+            }
+
+            return array_merge(quickSort($left, $sort_type, $column_key), array($array_input[0]), quickSort($right, $sort_type, $column_key));        
         }
 
-        return array_merge(quickSort($left, $array_sort_order, $column_key), array($array_input[0]), quickSort($right, $array_sort_order, $column_key));        
-      }
+        function createProduct($indexed = 10):array
+        {
+            $list_product = array();
+            $key_product = array("id", "name", "prince", "quantity", "order", "total");
+            for ($i=0; $i<$indexed; $i++) {
+                $prince = mt_rand(1000, 50000);
+                $quantity = mt_rand(1, 100);
+                $value_product = array($i, "Product " .str_pad($i, count($indexed)+1, '0', STR_PAD_LEFT), $prince ,$quantity ,mt_rand(1, 100), $prince*$quantity);
+                $product = array_combine($key_product, $value_product);
+                array_push($list_product, $product);
+            }
 
-      function createProduct():array
-      {
-        $list_product = array();
-        $key_product = array("id", "name", "prince", "quantity", "order", "total");
-        for ($i=0; $i<=9; $i++) {
-          $prince = mt_rand(1000, 50000);
-          $quantity = mt_rand(1, 100);
-          $value_product = array($i, "Product " .($i+1), $prince ,$quantity ,mt_rand(1, 100), $prince*$quantity);
-          $product = array_combine($key_product, $value_product);
-          array_push($list_product, $product);
+            return $list_product;
         }
 
-        $data_products = array('products' => $list_product, 'sort_type' => array('id' => SORT_ASC));
-        return $data_products;
-      }
+        function checkErr($number):array
+        {
+            if ($number == '') {
+                return array('stt' => 0, 'message' => 'Chưa nhập số lượng sản phẩm');
+            }
+            
+            if (!is_numeric($number) || $number - (int)$number != 0 || $number < 0) {
+                return array('stt' => 0, 'message' => 'Yêu cầu nhập số nguyên dương lớn hơn 0');
+            }
+           
+            return array('stt' => 1);
+        }
+
     ?>
 
-  </div>  
+</div>  
 </body>
 </html>
